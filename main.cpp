c17756124395e59e470926d69fbfbe8ae658b26a
bill-hscc <phongbinh2511@gmail.com>
2022-05-22 10:07:44 +0800

    [master] constructed simple classifiers.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <bits/stdc++.h>
+#include "XLessClassifier.h"
+#include "XMoreEqlClassifier.h"
+#include "YLessClassifier.h"
+#include "YMoreEqlClassifier.h"
 
 using namespace std;
 
 #define DATA_FILENAME "data.txt"
 #define MAX_DOUBLE_DIGITS_N 20
+#define MIN_X -5
+#define MIN_Y -5
 
 struct Sample {
   double x = 0.0;
@@ -12,6 +18,36 @@ struct Sample {
 };
 
 vector<Sample> samples;
+vector<Classifier*> classifierPtrs;
+
+void constructDecisionTreeStumps() {
+  vector<double> xs(samples.size());
+  vector<double> ys(samples.size());
+  for (int i = 0; i < samples.size(); ++i) {
+    xs[i] = samples[i].x;
+    ys[i] = samples[i].y;
+  }
+  sort(xs.begin(), xs.end());
+  sort(ys.begin(), ys.end());
+  {
+    double prevX = MIN_X;
+    for (auto x : xs) {
+      double threshold = (prevX + x) / 2.0;
+      classifierPtrs.push_back(new XLessClassifier(threshold));
+      classifierPtrs.push_back(new XMoreEqlClassifier(threshold));
+      prevX = x;
+    }
+  }
+  {
+    double prevY = MIN_Y;
+    for (auto y : ys) {
+      double threshold = (prevY + y) / 2.0;
+      classifierPtrs.push_back(new YLessClassifier(threshold));
+      classifierPtrs.push_back(new YMoreEqlClassifier(threshold));
+      prevY = y;
+    }
+  }
+}
 
 int main() {
   freopen(DATA_FILENAME, "r", stdin);
@@ -25,5 +61,6 @@ int main() {
     sample.r = r;
     samples.push_back(sample);
   }
+  constructDecisionTreeStumps();
   return 0;
 }
